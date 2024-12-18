3f32e3c9cfd5deda7cb1fea7def8f071266ef363
taro.kirameki <taro.kirameki@techacademy.jp>
2024-12-18 13:24:56 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="prose ml-4">
-        <h2>id = {{ $task->id }} のタスク詳細ページ</h2>
+        <h2 class ="text-lg">id = {{ $task->id }} のタスク詳細ページ</h2>
     </div>
 
     <table class="table w-full my-4">
@@ -11,7 +11,10 @@
             <th>id</th>
             <td>{{ $task->id }}</td>
         </tr>
-
+        <tr>
+            <th>status</th>
+            <td>{{ $task->status }}</td>
+        </tr>
         <tr>
             <th>メッセージ</th>
             <td>{{ $task->content }}</td>
