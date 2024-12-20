03cc1e843bc57a077174222f89022083adbffc24
taro.kirameki <taro.kirameki@techacademy.jp>
2024-12-20 11:53:59 +0000

    hanngakusyusei

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="prose ml-4">
-        <h2 class ="text-lg">id = {{ $task->id }} のタスク詳細ページ</h2>
+        <h2 class="text-lg">id = {{ $task->id }} タスクの詳細ページ</h2>
     </div>
 
     <table class="table w-full my-4">
